1b642c2a16f98ae6336291fd2b965b3e38db69b2
Mark Smith <markjgsmith@gmail.com>
2017-09-13 10:11:55 +0700

    Add tests for user retrieval

--- test/integration-api.test.js
+++ test/integration-api.test.js
@@ -6,6 +6,17 @@ function encodeCredentials(username, password) {
   return new Buffer(`${username}:${password}`).toString('base64');
 }
 
+function validateListOfUsers(list) {
+  list.forEach(function(item) {
+    validateUser(item);
+  });
+}
+
+function validateUser(obj) {
+  expect(obj.username).to.be.an('string');
+  expect(obj.age).to.be.a('number');
+}
+
 describe('Integration tests: api', function() {
   const username = 'mark';
   const password = 'password1';
@@ -22,6 +33,59 @@ describe('Integration tests: api', function() {
         expect(err).to.be.null;
         expect(res.body.token).to.be.a('string');
         expect(res.body.user).to.be.an('object');
+        done();
+      });
+  });
+
+  it('should retrieve a user for unauthenticated retrieval of user', function(done) {
+    const userUrl = `/api/users/mark`;
+    request(app)
+      .get(userUrl)
+      .set('Accept', 'application/json')
+      .set('Authorization', `Basic ${encodeCredentials(username, password)}`)
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        validateUser(res.body);
+        done();
+      });
+  });
+
+  it('should retrieve a user for authenticated retrieval of user', function(done) {
+    const userUrl = `/api/users/mark`;
+    request(app)
+      .get(userUrl)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        validateUser(res.body);
+        done();
+      });
+  });
+
+  it('should return 403 for unauthenticated retrieval of users', function(done) {
+    const usersUrl = `/api/users`;
+    request(app)
+      .get(usersUrl)
+      .set('Accept', 'application/json')
+      .expect(403)
+      .end(done);
+  });
+
+  it('should retrieve a list for users for authenticated retrieval of users', function(done) {
+    const usersUrl = `/api/users`;
+    request(app)
+      .get(usersUrl)
+      .set('Accept', 'application/json')
+      .set('Authorization', `Basic ${encodeCredentials(username, password)}`)
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        validateListOfUsers(res.body);
         done();
       });
   });
